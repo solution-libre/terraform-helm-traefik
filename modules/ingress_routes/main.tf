145a4b2a1c11521c30a7af625954dd9fc3165197
Florent Poinsaut <florent@poinsaut.fr>
2024-02-22 18:17:42 +0100

    Move Ingress route to a sub module

---
+++ modules/ingress_routes/main.tf
@@ -0,0 +1,28 @@
+/**
+ * Copyright (C) 2024 Solution Libre <contact@solution-libre.fr>
+ * 
+ * This file is part of Traefik Terraform module.
+ * 
+ * Traefik Terraform module is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Traefik Terraform module is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Traefik Terraform module.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+resource "kubernetes_manifest" "ingress_routes" {
+  manifest = yamldecode(templatefile(
+    "${path.module}/templates/manifests/ingress-route.yaml.tpl",
+    {
+      metadata = var.metadata
+      spec     = var.spec
+    }
+  ))
+}
